20edf017ce3f672e6ce5868d19bd37910f64800b
mriyanhidayat <riyanmh17@gmail.com>
2022-04-18 14:11:36 +0700

    Structure Complex with Web API

---
+++ WebAPI/NorthwindWebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Northwind.Contracts;
+using Northwind.LoggerService;
+
+namespace NorthwindWebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //depelopmen to postman open to access method POST, GET, DELETE, PUT
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        // add IIS configure options deploy to IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+
+        //create a service once per request
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
